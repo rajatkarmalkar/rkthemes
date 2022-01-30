c4e4c8f2b0266cb60f5feb578b6b3aa303236200
Rajat <rajat.karmalkar@gmail.com>
2022-01-30 10:51:26 +0530

    rkthemes initial staging

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if (!defined('RKTHEME_DIR_URI')) {
 }
 
 require_once  RKTHEME_DIR_PATH . '/inc/helpers/autoloader.php';
-
+require_once  RKTHEME_DIR_PATH . '/inc/helpers/template-tags.php';
 
 
  function rktheme_instance(){
@@ -23,20 +23,6 @@ require_once  RKTHEME_DIR_PATH . '/inc/helpers/autoloader.php';
 
 rktheme_instance();
 
-function add_scripts() {
-		//wp_register_style( 'style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/assets/css/style.css' ), 'all' );
-	//	wp_register_script('style', get_template_directory_uri() . 'assets/js/script.js', [], filemtime( get_template_directory() . '/assets/js/script.css' ), 'all');
-
-// 	$myfile = get_template_directory_uri() . '/assets/css/cd-custom.css';
-// $realpath = get_template_directory().'/assets/css/cd-custom.css';
-// $myfilejs = get_template_directory_uri() . '/assets/js/main.js';
-// $realpathjs = get_template_directory().'/assets/js/main.js';
-
-		
-
-			
-}
-add_action("wp_enqueue_scripts" , "add_scripts");
 
 
- ?>
+
--- index.php
+++ index.php
@@ -1,16 +1,71 @@
 <?php 
+/**
+* Main theme file
+*
+*    @package rktheme
+*/
 
 get_header();
  ?>
 
+	<div id="primary">
+		<main id="main" class="site-main mt-5" role="main">
+			<?php
+			if ( have_posts() ) :
+				?>
+				<div class="container">
+					<?php
+					if ( is_home() && ! is_front_page() ) {
+						?>
+						<header class="mb-5">
+							<h1 class="page-title screen-reader-text">
+								<?php single_post_title(); ?>
+							</h1>
+						</header>
+						<?php
+					}
+					?>
 
-<?php 	if ( have_posts() ) : 
-    while ( have_posts() ) : the_post();
-        the_content();
-    endwhile;
-else :
-    _e( 'Sorry, no posts matched your criteria.', 'rkthemes' );
-endif; ?>
+					<div class="row">
+						<?php
+						$index         = 0;
+						$no_of_columns = 3;
+
+						while ( have_posts() ) : the_post();
+
+							if ( 0 === $index % $no_of_columns ) {
+								?>
+								<div class="col-lg-4 col-md-6 col-sm-12">
+								<?php
+							}
+
+							get_template_part( 'template-parts/content' );
+
+							$index ++;
+
+							if ( 0 !== $index && 0 === $index % $no_of_columns ) {
+								?>
+								</div>
+								<?php
+							}
+
+						endwhile;
+						?>
+					</div>
+				</div>
+			<?php
+
+			else :
+
+				get_template_part( 'template-parts/content-none' );
+
+			endif;
+
+			rkthemes_pagination();
+			?>
+		</main>
+	</div>
+
 
  <?php 
 
